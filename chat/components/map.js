bfed46cc6f4ee9cc252f472bf3ea2a3be143f761
Mateusz Sikora <mateusz.sikora@nokia.com>
2016-06-29 17:27:41 +0200

    fix map

--- chat/components/map.js
+++ chat/components/map.js
@@ -11,60 +11,28 @@ import {
 import MapView from 'react-native-maps';
 
 export default class Map extends Component {
-
-  fakeMarkers = [{
-    coordinates: {
-      latitude: 37.78825,
-      longitude: -122.4324
-    },
-    title: 'title 1',
-    description: 'description 1'
-  }];
-
   constructor() {
     super();
   }
 
   render() {
-    return (
-      <View style={styles.container}>
-        <MapView
-          style={styles.map}
-          initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421
-          }}>
-          {this.fakeMarkers.map(marker => (
-            <MapView.Marker
-              coordinate={marker.coordinates}
-              title={marker.title}
-              description={marker.description}
-            />
-          ))}
-        </MapView>
-    </View>);
+    return (<View style={styles.container}><MapView
+      style={styles.map}
+      initialRegion={{
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421
+      }}
+    /></View>);
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      right: 0,
-      bottom: 0,
-      height: 400,
-      width: 400,
-      justifyContent: 'flex-end',
-      alignItems: 'center',
-    },
+    flex: 1
+  },
   map: {
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
-        },
+    flex: 1
+  }
 });
